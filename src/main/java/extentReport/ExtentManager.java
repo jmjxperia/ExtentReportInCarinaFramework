5fc5bc0a104afbebdecdb665e3b96c571d126b1f
jmjxperia <jmjxperia@users.noreply.github.com>
2021-09-11 14:34:29 +0530

    Method to handle date and time

--- src/main/java/extentReport/ExtentManager.java
+++ src/main/java/extentReport/ExtentManager.java
@@ -2,15 +2,25 @@ package extentReport;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ExtentManager {
    private static ExtentReports extent;
    public static final String REPORT_FILE_PATH ="./Automation_Reports/";
 
+   public static String dateGenerator()
+   {
+       DateFormat dateFormat = new SimpleDateFormat("dd_MMM_yyyy_HH_mm_ss");
+       Date date = new Date();
+       return dateFormat.format(date);
+   }
+
    public static void createInstance()
    {
        String path=System.getProperty("user.dir")+REPORT_FILE_PATH;
-       ExtentSparkReporter reporter = new ExtentSparkReporter(path+"Test-Automaton-Report"+"_"+".html");
+       ExtentSparkReporter reporter = new ExtentSparkReporter(path+"Test-Automaton-Report"+"_"+dateGenerator()+".html");
        reporter.config().setReportName("Automation Results");
        reporter.config().setDocumentTitle("Test Results");
        //reporter.config().setTheme(Theme.DARK);
